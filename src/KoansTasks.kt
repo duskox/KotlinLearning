fefc44d987f499a785c5a13755a37b9b0e13c5bd
Dusan Vorkapic <duskox@gmail.com>
2017-07-04 18:27:51 +0200

    Read up on extension functions

--- src/KoansTasks.kt
+++ src/KoansTasks.kt
@@ -4,3 +4,68 @@ package dv.temporary
  */
 
 fun start() : String = "OK"
+
+
+// Named arguments - solution
+fun joinOptions(options: Collection<String>) = options.joinToString(", ", "[", "]");
+
+// Default arguments - solution
+// It was neccessary to add default parameters to number, toUpperCase)
+fun foo(name: String, number: Int = 42, toUpperCase: Boolean = false) =
+        (if (toUpperCase) name.toUpperCase() else name) + number
+
+fun useFoo() = listOf(
+        foo("a"),
+        foo("b", number = 1),
+        foo("c", toUpperCase = true),
+        foo(name = "d", number = 2, toUpperCase = true)
+)
+
+// Lambdas
+fun containsEven(collection: Collection<Int>): Boolean = collection.any { x -> x%2 == 0 }
+
+
+// Strings
+val month = "(JAN|FEB|MAR|APR|MAY|JUN|JUL|AUG|SEP|OCT|NOV|DEC)"
+fun getPattern(): String = """\d{2} ${month} \d{4}"""
+
+// Data classes
+data class Person (val name :String = "", val age :Int = 0)
+fun getPeople(): List<Person> {
+    return listOf(Person("Alice", 29), Person("Bob", 31))
+}
+
+// Nullable types
+fun sendMessageToClient(
+        client: Client?, message: String?, mailer: Mailer
+){
+    var personalInfo : PersonalInfo? = client?.personalInfo;
+    var email : String? = personalInfo?.email;
+
+    if(email == null || message == null) return
+    mailer.sendMessage(email, message);
+}
+
+class Client (val personalInfo: PersonalInfo?)
+class PersonalInfo (val email: String?)
+interface Mailer {
+    fun sendMessage(email: String, message: String)
+}
+
+
+// Smart casts
+fun eval(expr: Expr): Int =
+        when (expr) {
+            is Num -> expr.value;
+            is Sum -> eval(expr.left) + eval(expr.right);
+            else -> throw IllegalArgumentException("Unknown expression")
+        }
+
+interface Expr
+class Num(val value: Int) : Expr
+class Sum(val left: Expr, val right: Expr) : Expr
+
+// Extension functions
+fun Int.r(): RationalNumber = RationalNumber(this, 1)
+fun Pair<Int, Int>.r(): RationalNumber = RationalNumber(first, second)
+data class RationalNumber(val numerator: Int, val denominator: Int)
--- src/dv/temporary/Main.java
+++ src/dv/temporary/Main.java
@@ -1,17 +1,60 @@
 package dv.temporary;
+
+import com.sun.deploy.util.ArrayUtil;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 /**
  * Created by DV on 04.07.17.
  */
 public class Main {
     public static void main(String[] args) {
-       prnt(KoansTasksKt.start());
+        prntLn(KoansTasksKt.start());
+
+        String[] arrStr = {"1", "2", "3"};
+        List<String> arrayList = Arrays.asList(arrStr);
+        prntLn("Named arguments:");
+        prntLn(KoansTasksKt.joinOptions(arrayList));
+
+        prntLn("Default arguments:");
+        prntLn(KoansTasksKt.useFoo().toString());
+
+        Integer[] arr = {1, 2, 3, 4};
+        List<Integer> list = Arrays.asList(arr);
+        prntLn("Contains even (1, 2, 3, 4):");
+        prntLn(KoansTasksKt.containsEven(Arrays.asList(arr)));
+
+        prntLn("Strings:");
+        prntLn(KoansTasksKt.getPattern());
+
+        prntLn("Data classes:");
+        prntLn(KoansTasksKt.getPeople().toString());
+
+        prntLn("Nullable types:");
+        KoansTasksKt.sendMessageToClient(new Client(new PersonalInfo("emailParam")), "messageParam", new Mailer() {
+            @Override
+            public void sendMessage(@NotNull String email, @NotNull String message) {
+                prntLn("email:" + email + " message:" + message);
+            }
+        });
+
+        prntLn("Smart casts:");
+        prntLn(KoansTasksKt.eval(new Num(1)));
+        prntLn(KoansTasksKt.eval(new Sum(new Num(2), new Num(5))));
+
+
     }
 
-    private static void prnt(String s) {
+    private static <T> void prnt(T s) {
         System.out.print(s);
     }
 
-    private static void prntLn(String s) {
+    private static <T> void prntLn(T s) {
         System.out.println(s);
     }
 }
